d87e50c071f13a3b48f4527c070d5e0158461202
senatopal <senatopal02@hotmail.com>
2026-07-29 17:37:52 +0300

    deploy icin guncellendi

--- frontend/rxconfig.py
+++ frontend/rxconfig.py
@@ -1,3 +1,4 @@
+import os
 import reflex as rx
 
 
@@ -6,8 +7,8 @@ config = rx.Config(
     frontend_port=3000,
     backend_port=8001,
     backend_host="0.0.0.0",
-    api_url="http://127.0.0.1:8001",
-    deploy_url="http://127.0.0.1:3000",
+    api_url=os.getenv("API_URL", "http://127.0.0.1:8001"),
+    deploy_url=os.getenv("DEPLOY_URL", "http://127.0.0.1:3000"),
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.RadixThemesPlugin(
