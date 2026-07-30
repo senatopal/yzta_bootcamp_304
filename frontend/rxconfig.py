42bbc48aec0364cf1ed64f15b6c0eaa0d13709eb
senatopal <senatopal02@hotmail.com>
2026-07-30 16:53:27 +0300

    update rxconfig.py

--- frontend/rxconfig.py
+++ frontend/rxconfig.py
@@ -9,7 +9,7 @@ DEPLOY_URL = os.getenv("DEPLOY_URL", "https://yzta-bootcamp-304-2.onrender.com")
 config = rx.Config(
     app_name="reflex_frontend",
     frontend_port=port,
-    backend_port=8001,
+    backend_port=port,  
     backend_host="0.0.0.0",
     api_url=API_URL,
     deploy_url=DEPLOY_URL,
@@ -23,7 +23,6 @@ config = rx.Config(
         ),
     ],
 )
-
 # import os
 # import reflex as rx
 
