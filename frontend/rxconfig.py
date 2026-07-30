11555a6e359e25af30c42a0cce44bffbfa338cd0
senatopal <senatopal02@hotmail.com>
2026-07-30 15:56:59 +0300

    update rxconfig.py

--- frontend/rxconfig.py
+++ frontend/rxconfig.py
@@ -1,22 +1,18 @@
 import os
 import reflex as rx
 
-# Render veya deployment ortamından portları ve URL'leri al
-port = os.getenv("PORT")
-frontend_port = int(port) if port is not None else 3000
-backend_port = int(port) if port is not None else 8001
+port = int(os.getenv("PORT", 3000))
 
-# Backend canlı URL'i (Örn: https://yzta-bootcamp-304-1.onrender.com)
-api_url = os.getenv("API_URL", f"http://127.0.0.1:{backend_port}")
-deploy_url = os.getenv("DEPLOY_URL", f"http://127.0.0.1:{frontend_port}")
+API_URL = os.getenv("API_URL", "https://yzta-bootcamp-304-1.onrender.com")
+DEPLOY_URL = os.getenv("DEPLOY_URL", "https://yzta-bootcamp-304-2.onrender.com")
 
 config = rx.Config(
     app_name="reflex_frontend",
-    frontend_port=frontend_port,
-    backend_port=backend_port,
+    frontend_port=port,
+    backend_port=8001,
     backend_host="0.0.0.0",
-    api_url=api_url,
-    deploy_url=deploy_url,
+    api_url=API_URL,
+    deploy_url=DEPLOY_URL,
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.RadixThemesPlugin(
@@ -27,7 +23,6 @@ config = rx.Config(
         ),
     ],
 )
-
 
 # import os
 # import reflex as rx
