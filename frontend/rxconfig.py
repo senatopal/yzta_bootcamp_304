40ad0202847d10edbeadb308a0b49d179b12856e
senatopal <senatopal02@hotmail.com>
2026-07-30 13:38:48 +0300

    url updated in rxconfig for deploying

--- frontend/rxconfig.py
+++ frontend/rxconfig.py
@@ -1,14 +1,19 @@
 import os
 import reflex as rx
 
+port = os.getenv("PORT")
+frontend_port = int(port) if port is not None else 3000
+backend_port = int(port) if port is not None else 8001
+
+default_deploy_url = f"http://127.0.0.1:{frontend_port}"
 
 config = rx.Config(
     app_name="reflex_frontend",
-    frontend_port=3000,
-    backend_port=8001,
+    frontend_port=frontend_port,
+    backend_port=backend_port,
     backend_host="0.0.0.0",
     api_url=os.getenv("API_URL", "http://127.0.0.1:8001"),
-    deploy_url=os.getenv("DEPLOY_URL", "http://127.0.0.1:3000"),
+    deploy_url=os.getenv("DEPLOY_URL", default_deploy_url),
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.RadixThemesPlugin(
@@ -19,3 +24,27 @@ config = rx.Config(
         ),
     ],
 )
+
+
+
+# import os
+# import reflex as rx
+
+
+# config = rx.Config(
+#     app_name="reflex_frontend",
+#     frontend_port=3000,
+#     backend_port=8001,
+#     backend_host="0.0.0.0",
+#     api_url=os.getenv("API_URL", "http://127.0.0.1:8001"),
+#     deploy_url=os.getenv("DEPLOY_URL", "http://127.0.0.1:3000"),
+#     plugins=[
+#         rx.plugins.SitemapPlugin(),
+#         rx.plugins.RadixThemesPlugin(
+#             theme=rx.theme(
+#                 appearance="light",
+#                 accent_color="teal",
+#             )
+#         ),
+#     ],
+# )
